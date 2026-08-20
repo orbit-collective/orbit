9dc99f1507a60473fc9f2ce71d7c8b9c752c2db9
Adiksuu <codeadiksuu@gmail.com>
2026-08-20 19:16:10 +0200

    feat(permissions): add enums for permissions and role types

---
+++ app/Enums/Permissions/RoleType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums\Permissions;
+
+enum RoleType: string
+{
+    case OWNER = 'owner';
+    case ADMIN = 'admin';
+    case MEMBER = 'member';
+    case VIEWER = 'viewer';
+}
