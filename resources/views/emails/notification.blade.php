877b0ba8b99000790374ec85b9deff42dd931d21
Adiksuu <codeadiksuu@gmail.com>
2026-08-24 19:04:02 +0200

    style(emails): update email templates for light mode support

--- resources/views/emails/notification.blade.php
+++ resources/views/emails/notification.blade.php
@@ -3,9 +3,9 @@
 @section('subject', $title)
 
 @section('content')
-    <h1 style="margin:0 0 16px; font-size:20px; line-height:28px; color:#f7f7f8;">Hello {{ $notifiable->name }}!</h1>
+    <h1 class="email-text" style="margin:0 0 16px; font-size:20px; line-height:28px; color:#f7f7f8;">Hello {{ $notifiable->name }}!</h1>
 
-    <p style="margin:0 0 24px; color:#f7f7f8;">{{ $body }}</p>
+    <p class="email-text" style="margin:0 0 24px; color:#f7f7f8;">{{ $body }}</p>
 
     @if($actionUrl)
         @include('emails.partials.button', ['url' => $actionUrl, 'text' => 'View in Orbit'])
