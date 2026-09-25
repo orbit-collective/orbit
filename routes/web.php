221c445679a0df9776a65379c19d75124506bf29
Adiksuu <codeadiksuu@gmail.com>
2026-09-25 17:17:48 +0200

    feat(github): add repository management endpoints and routes

--- routes/web.php
+++ routes/web.php
@@ -79,6 +79,9 @@ Route::middleware('auth')->group(function () {
     Route::post('/projects/{project}/integrations/github/connect', [GithubIntegrationController::class, 'connect'])->name('projects.integrations.github.connect');
     Route::post('/projects/{project}/integrations/github/disconnect', [GithubIntegrationController::class, 'disconnect'])->name('projects.integrations.github.disconnect');
     Route::post('/projects/{project}/integrations/github/retry', [GithubIntegrationController::class, 'retry'])->name('projects.integrations.github.retry');
+    Route::post('/projects/{project}/integrations/github/repositories/sync', [GithubIntegrationController::class, 'syncRepositories'])->name('projects.integrations.github.repositories.sync');
+    Route::post('/projects/{project}/integrations/github/repositories', [GithubIntegrationController::class, 'addRepository'])->name('projects.integrations.github.repositories.store');
+    Route::delete('/projects/{project}/integrations/github/repositories/{repositoryId}', [GithubIntegrationController::class, 'removeRepository'])->name('projects.integrations.github.repositories.destroy');
     Route::post('/projects/{project}/invitations', [ProjectInvitationController::class, 'store'])->name('projects.invitations.store');
     Route::delete('/projects/{project}/invitations/{invitation}', [ProjectInvitationController::class, 'destroy'])->name('projects.invitations.destroy');
     Route::post('/invitations/accept', [ProjectInvitationController::class, 'acceptManual'])->name('invitations.accept-manual');
